a28710dccf7a025088e93a25ee6b656b430e0546
AxelOjedaHernandez <axelojedahernandez64@gmail.com>
2025-01-14 12:18:27 -0600

    septimo

--- src/main/java/com/agoh/backend/config/SecurityConfig.java
+++ src/main/java/com/agoh/backend/config/SecurityConfig.java
@@ -15,9 +15,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.agoh.backend.services.UserDetailServiceImpl;
 
@@ -30,33 +27,28 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Integrar configuración de CORS
+                .cors(Customizer.withDefaults()) // Habilitar CORS desde CorsConfig
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(http -> {
+                    // Configurar los endpoints públicos
                     http.requestMatchers(
                             "/swagger-ui/**", "/v3/api-docs/**", "/api/auth/login").permitAll();
+                    
+                    // Permitir solicitudes OPTIONS para preflight requests
                     http.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+
+                    // Configurar los endpoints privados
                     http.requestMatchers(HttpMethod.GET, "/api/v1/promotion/listar").hasAuthority("READ");
                     http.requestMatchers(HttpMethod.GET, "/api/v1/promotion/listar/{id}").hasAuthority("READ");
                     http.requestMatchers(HttpMethod.POST, "/api/v1/promotion/crear").hasAuthority("CREATE");
                     http.requestMatchers(HttpMethod.PUT, "/api/v1/promotion/actualizar/{id}").hasAuthority("UPDATE");
                     http.requestMatchers(HttpMethod.DELETE, "/api/v1/promotion/eliminar/{id}").hasAuthority("DELETE");
+
+                    // Configurar el resto de endpoints - Requiere autenticación
                     http.anyRequest().authenticated();
                 })
                 .build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("https://examen3-front.vercel.app"); // Configura tu URL específica
-        configuration.addAllowedMethod("*"); // Permite todos los métodos HTTP
-        configuration.addAllowedHeader("*"); // Permite todos los headers
-        configuration.setAllowCredentials(true); // Permite envío de cookies/credenciales
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 
     @Bean
